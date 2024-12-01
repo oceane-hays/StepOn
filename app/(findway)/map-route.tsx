79827f291ef430452b6e00da3e7150b4aff227c5
samuel <leumasmontreal@hotmail.com>
2024-12-01 15:06:33 -0500

    Async Storage Timer

--- app/(findway)/map-route.tsx
+++ app/(findway)/map-route.tsx
@@ -1,5 +1,11 @@
-import React, {useEffect, useRef, useState} from "react";
-import { SafeAreaView, View, StyleSheet, TouchableOpacity, Text } from "react-native";
+import React, { useEffect, useRef, useState } from "react";
+import {
+  SafeAreaView,
+  View,
+  StyleSheet,
+  TouchableOpacity,
+  Text,
+} from "react-native";
 import MapView, { Marker, Region } from "react-native-maps";
 import { LATITUDE_DELTA } from "@/services/LATITUDE_DELTA";
 import { LONGITUDE_DELTA } from "@/services/LONGITUDE_DELTA";
@@ -8,163 +14,238 @@ import { CircularItems } from "@/app/(component)/RouteComponent/CircularItems";
 import { Footprints, MapPin, Timer } from "lucide-react-native";
 import { GenerateRoundTrip } from "@/app/(component)/RouteComponent/generateRoute";
 import MapViewDirections from "react-native-maps-directions";
-import {GOOGLE_MAPS_API_KEY} from "@/services/GOOGLE_MAPS_API_KEY";
-import {Destination} from "@/app/(component)/RouteComponent/types";
+import { GOOGLE_MAPS_API_KEY } from "@/services/GOOGLE_MAPS_API_KEY";
+import { Destination } from "@/app/(component)/RouteComponent/types";
+import { AppState } from "react-native";
+import { differenceInSeconds } from "date-fns";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-
-const DEFAULT_LOCATION : Region = {
-    latitude: 45.48833488659076,
-    longitude: -73.63675359307672,
-    latitudeDelta: LATITUDE_DELTA,
-    longitudeDelta: LONGITUDE_DELTA,
+const DEFAULT_LOCATION: Region = {
+  latitude: 45.48833488659076,
+  longitude: -73.63675359307672,
+  latitudeDelta: LATITUDE_DELTA,
+  longitudeDelta: LONGITUDE_DELTA,
 };
 
 const route = GenerateRoundTrip(DEFAULT_LOCATION, 1.5);
 
 export default function MapRoute() {
-    const mapRef = useRef<MapView>(null);
-    const [destinations, setDestinations] = useState<Destination[]>([]);
+  const mapRef = useRef<MapView>(null);
+  const [destinations, setDestinations] = useState<Destination[]>([]);
 
+  const appState = useRef(AppState.currentState);
+  const [elapsed, setElapsed] = useState<number | undefined>(0);
 
-    useEffect(() => {
-        const newDestinations: Destination[] = route.map(point => {
-            if ('center' in point && point.center) {
-                return point.center;
-            } else {
-                return { latitude: point.latitude, longitude: point.longitude };
-            }
-        });
-        setDestinations(newDestinations);
-    }, []);
+  const recordStartTime = async () => {
+    try {
+      const now = new Date();
+      await AsyncStorage.setItem("@start_time", now.toISOString());
+    } catch (err) {
+      // TODO: handle errors from setItem properly
+      console.warn(err);
+    }
+  };
 
-    const handleTakeABreak = () => {
-        console.log("Take a Break pressed");
-        // Add your logic for taking a break
-    };
+  useEffect(() => {
+    const interval = setInterval(async () => {
+      const elapsedTime = await getElapsedTime();
+      setElapsed(elapsedTime ?? 0);
+    }, 1000);
 
-    const handleFinish = () => {
-        console.log("Finish pressed");
-        // Add your logic for finishing the route
-    };
+    // Clear the interval when the component unmounts
+    return () => clearInterval(interval);
+  }, []);
 
-    return (
-        <SafeAreaView style={styles.container}>
-            <View style={styles.mapContainer}>
-                <MapView
-                    ref={mapRef}
-                    provider={MapView.PROVIDER_GOOGLE}
-                    style={styles.map}
-                    initialRegion={{
-                        latitude: route[0].latitude,
-                        longitude: route[0].longitude,
-                        latitudeDelta: route[0].latitudeDelta,
-                        longitudeDelta: route[0].longitudeDelta,
-                    }}
-                >
-                    {destinations.map((point, index) => (
-                        <Marker
-                            key={index}
-                            coordinate={{ latitude: point.latitude, longitude: point.longitude }}
-                            title={`Point ${index}`}
-                        />
-                    ))}
-                    {destinations.map((destination, index) => {
-                        if (index === 0) return null;
-                        return (
-                            <MapViewDirections
-                                key={index}
-                                origin={destinations[index - 1]}
-                                destination={destination}
-                                apikey={GOOGLE_MAPS_API_KEY}
-                                strokeColor="orange"
-                                strokeWidth={4}
-                                onError={(error) => console.log("Directions error:", error)}
-                            />
-                        );
-                    })}
-                </MapView>
+  useEffect(() => {
+    AppState.addEventListener("change", handleAppStateChange);
+    recordStartTime();
+    return;
+  }, []);
 
-                <View style={styles.cardContainer}>
-                    <View style={{...styles.card, padding: 20, width: '85%', alignSelf:'center'}}>
-                        <CircularItems icon={<Footprints color={Colors.orange_fonce} />} numb={0} pourcent={10} />
-                        <CircularItems icon={<Timer color={Colors.orange_fonce} />} numb={0} pourcent={30} />
-                        <CircularItems icon={<MapPin color={Colors.orange_fonce} />} numb={0} pourcent={60} />
-                    </View>
+  const handleAppStateChange = async (nextAppState: any) => {
+    if (
+      appState.current.match(/inactive|background/) &&
+      nextAppState === "active"
+    ) {
+      // We just became active again: recalculate elapsed time based
+      // on what we stored in AsyncStorage when we started.
+      const elapsed = await getElapsedTime();
+      // Update the elapsed seconds state
+      console.log(elapsed);
+      setElapsed(elapsed);
+    }
+    appState.current = nextAppState;
+  };
+  const getElapsedTime = async () => {
+    try {
+      const startTime = await AsyncStorage.getItem("@start_time");
+      const now = new Date();
+      if (startTime) return differenceInSeconds(now, Date.parse(startTime));
+    } catch (err) {
+      // TODO: handle errors from setItem properly
+      console.warn(err);
+    }
+  };
 
-                    <View style={styles.divider} />
+  useEffect(() => {
+    const newDestinations: Destination[] = route.map((point) => {
+      if ("center" in point && point.center) {
+        return point.center;
+      } else {
+        return { latitude: point.latitude, longitude: point.longitude };
+      }
+    });
+    setDestinations(newDestinations);
+  }, []);
 
-                    <View style={styles.card}>
-                        <TouchableOpacity
-                            style={[styles.button, { backgroundColor: Colors.bleu_clair }]}
-                            onPress={handleTakeABreak}
-                        >
-                            <Text style={styles.buttonText}>Take a Break</Text>
-                        </TouchableOpacity>
-                        <TouchableOpacity
-                            style={[styles.button, { backgroundColor: Colors.orange_fonce }]}
-                            onPress={handleFinish}
-                        >
-                            <Text style={styles.buttonText}>Finish</Text>
-                        </TouchableOpacity>
-                    </View>
-                </View>
-            </View>
-        </SafeAreaView>
-    );
+  const handleTakeABreak = () => {
+    console.log("Take a Break pressed");
+    // Add your logic for taking a break
+  };
+
+  const handleFinish = () => {
+    console.log("Finish pressed");
+  };
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.mapContainer}>
+        <MapView
+          ref={mapRef}
+          provider={MapView.PROVIDER_GOOGLE}
+          style={styles.map}
+          initialRegion={{
+            latitude: route[0].latitude,
+            longitude: route[0].longitude,
+            latitudeDelta: route[0].latitudeDelta,
+            longitudeDelta: route[0].longitudeDelta,
+          }}
+        >
+          {destinations.map((point, index) => (
+            <Marker
+              key={index}
+              coordinate={{
+                latitude: point.latitude,
+                longitude: point.longitude,
+              }}
+              title={`Point ${index}`}
+            />
+          ))}
+          {destinations.map((destination, index) => {
+            if (index === 0) return null;
+            return (
+              <MapViewDirections
+                key={index}
+                origin={destinations[index - 1]}
+                destination={destination}
+                apikey={GOOGLE_MAPS_API_KEY}
+                strokeColor="orange"
+                strokeWidth={4}
+                onError={(error) => console.log("Directions error:", error)}
+              />
+            );
+          })}
+        </MapView>
+
+        <View style={styles.cardContainer}>
+          <View
+            style={{
+              ...styles.card,
+              padding: 20,
+              width: "85%",
+              alignSelf: "center",
+            }}
+          >
+            <CircularItems
+              icon={<Footprints color={Colors.orange_fonce} />}
+              numb={0}
+              pourcent={10}
+            />
+            <CircularItems
+              icon={<Timer color={Colors.orange_fonce} />}
+              numb={elapsed ?? 0}
+              pourcent={30}
+            />
+            <CircularItems
+              icon={<MapPin color={Colors.orange_fonce} />}
+              numb={0}
+              pourcent={60}
+            />
+          </View>
+
+          <View style={styles.divider} />
+
+          <View style={styles.card}>
+            <TouchableOpacity
+              style={[styles.button, { backgroundColor: Colors.bleu_clair }]}
+              onPress={handleTakeABreak}
+            >
+              <Text style={styles.buttonText}>Take a Break</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={[styles.button, { backgroundColor: Colors.orange_fonce }]}
+              onPress={handleFinish}
+            >
+              <Text style={styles.buttonText}>Finish</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-    },
-    mapContainer: {
-        flex: 1,
-    },
-    map: {
-        ...StyleSheet.absoluteFillObject,
-    },
-    cardContainer: {
-        borderRadius: 20,
-        backgroundColor: "white",
-        position: "absolute",
-        bottom: 20,
-        left: 15,
-        right: 15,
-        shadowColor: "#000",
-        shadowOffset: { width: 0, height: 2 },
-        shadowOpacity: 0.25,
-        shadowRadius: 3.84,
-        elevation: 5,
-    },
-    card: {
-        padding: 15,
-        flexDirection: "row",
-        justifyContent: "space-between",
-        alignItems: 'center',
-    },
-    buttonText: {
-        fontSize: 16,
-        color: '#fff',
-        fontWeight: "bold",
-    },
-    divider: {
-        alignSelf: 'center',
-        height: 1,
-        backgroundColor: '#ccc',
-        width: '90%',
-    },
-    button: {
-        flex: 1,
-        alignItems: "center",
-        justifyContent: "center",
-        marginHorizontal: 5,
-        borderRadius: 20,
-        padding: 15,
-        shadowColor: Colors.gris_fonce,
-        shadowOffset: { width: 0, height: 1 },
-        shadowOpacity: 0.3,
-        shadowRadius: 3,
-        elevation: 2,
-    },
+  container: {
+    flex: 1,
+  },
+  mapContainer: {
+    flex: 1,
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+  cardContainer: {
+    borderRadius: 20,
+    backgroundColor: "white",
+    position: "absolute",
+    bottom: 20,
+    left: 15,
+    right: 15,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+  card: {
+    padding: 15,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  buttonText: {
+    fontSize: 16,
+    color: "#fff",
+    fontWeight: "bold",
+  },
+  divider: {
+    alignSelf: "center",
+    height: 1,
+    backgroundColor: "#ccc",
+    width: "90%",
+  },
+  button: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    marginHorizontal: 5,
+    borderRadius: 20,
+    padding: 15,
+    shadowColor: Colors.gris_fonce,
+    shadowOffset: { width: 0, height: 1 },
+    shadowOpacity: 0.3,
+    shadowRadius: 3,
+    elevation: 2,
+  },
 });
-
--- app/(findway)/plan-route.tsx
+++ app/(findway)/plan-route.tsx
@@ -1,12 +1,16 @@
-import React, { useState } from 'react';
-import {View, Text, StyleSheet, TouchableOpacity, SafeAreaView} from 'react-native';
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  SafeAreaView,
+} from "react-native";
 import Logo from "@/app/(component)/logo";
 import Filter from "@/app/(component)/filter";
-import {Colors} from "@/services/COLORS";
-import {useNavigation, useRouter} from "expo-router";
-import {NavigationProp} from "@react-navigation/core";
-
-
+import { Colors } from "@/services/COLORS";
+import { useNavigation, useRouter } from "expo-router";
+import { NavigationProp } from "@react-navigation/core";
 
 export default function PlanRoute() {
   const [steps, setSteps] = useState(1000);
@@ -23,133 +27,131 @@ export default function PlanRoute() {
   ) {
     // You can pass parameters to the route if needed
     router.push({
-        pathname: '/choose-route',
-        params: { steps, scenery, routeType, destination }
+      pathname: "/choose-route",
+      params: { steps, scenery, routeType, destination },
     });
   }
-
 
   const incrementSteps = () => setSteps((prev) => Math.min(prev + 1000, 10000));
   const decrementSteps = () => setSteps((prev) => Math.max(prev - 1000, 1000));
 
   return (
-        <SafeAreaView style={styles.container}>
-            <View>
-                <View style={{ marginBottom: 90 }}>
-                    <Logo />
-                </View>
+    <SafeAreaView style={styles.container}>
+      <View>
+        <View style={{ marginBottom: 90 }}>
+          <Logo />
+        </View>
 
-                {/* Header */}
-                <View style={styles.header}>
-                    <Text style={styles.title}>Plan Your Route</Text>
-                    <View style={styles.placeholder} />
-                </View>
-            </View>
+        {/* Header */}
+        <View style={styles.header}>
+          <Text style={styles.title}>Plan Your Route</Text>
+          <View style={styles.placeholder} />
+        </View>
+      </View>
 
-            <View style={{
-                ...card,
-                flex : 1,
-                justifyContent: 'space-around',
-                backgroundColor: Colors.gris,
-                paddingVertical: 10,
-                paddingHorizontal: 10,
-                borderRadius: 20,
-            }}>
-                <Filter
-                    incrementSteps={incrementSteps}
-                    decrementSteps={decrementSteps}
-                    steps={steps}
-                    scenery={scenery}
-                    setScenery={setScenery}
-                    routeType={routeType}
-                    setRouteType={setRouteType}
-                />
+      <View
+        style={{
+          ...card,
+          flex: 1,
+          justifyContent: "space-around",
+          backgroundColor: Colors.gris,
+          paddingVertical: 10,
+          paddingHorizontal: 10,
+          borderRadius: 20,
+        }}
+      >
+        <Filter
+          incrementSteps={incrementSteps}
+          decrementSteps={decrementSteps}
+          steps={steps}
+          scenery={scenery}
+          setScenery={setScenery}
+          routeType={routeType}
+          setRouteType={setRouteType}
+        />
 
-                {/* Action Buttons */}
-                <View style={styles.row}>
-                    <TouchableOpacity style={[styles.actionButton, styles.outlineButton]}>
-                        <Text style={styles.outlineButtonText}>Skip</Text>
-                    </TouchableOpacity>
+        {/* Action Buttons */}
+        <View style={styles.row}>
+          <TouchableOpacity
+            style={[styles.actionButton, styles.outlineButton]}
+            onPress={() => SubmitRoute(0, null, null, null)}
+          >
+            <Text style={styles.outlineButtonText}>Skip</Text>
+          </TouchableOpacity>
 
-                    <TouchableOpacity
-                        style={styles.actionButton}
-                        onPress={() =>
-                            SubmitRoute(steps, scenery, routeType, null)
-                        }
-                    >
-                        <Text style={styles.actionButtonText}>Submit</Text>
-                    </TouchableOpacity>
-                </View>
-            </View>
-
-        </SafeAreaView>
+          <TouchableOpacity
+            style={styles.actionButton}
+            onPress={() => SubmitRoute(steps, scenery, routeType, null)}
+          >
+            <Text style={styles.actionButtonText}>Submit</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </SafeAreaView>
   );
 }
 
-
-
 const card = {
-    marginHorizontal: 20,
-    marginBottom: 30,
-}
+  marginHorizontal: 20,
+  marginBottom: 30,
+};
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-    },
-    header: {
-        ...card,
-        alignItems: 'center',
-        justifyContent: 'space-between',
-        marginBottom: 20,
-    },
-    title: {
-        fontSize: 24,
-        fontWeight: 'bold',
-        color: Colors.orange_fonce,
-    },
-    iconButton: {
-        padding: 8,
-        borderRadius: 4,
-        backgroundColor: '#f0f0f0',
-        marginRight: 12,
-    },
-    row: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'space-between',
-    },
-    actionButton: {
-        flex: 1,
-        padding: 16,
-        borderRadius: 4,
-        backgroundColor: Colors.orange_fonce,
-        alignItems: 'center',
-        marginHorizontal: 4,
-    },
-    actionButtonText: {
-        fontSize: 16,
-        color: '#fff',
-    },
-    outlineButton: {
-        backgroundColor: '#f0f0f0',
-    },
-    outlineButtonText: {
-        fontSize: 16,
-        color: Colors.orange_fonce,
-    },
-    button: {
-        borderRadius: 10,
-        backgroundColor: '#fff',
-        padding: 10,
-        shadowColor: Colors.gris_fonce,
-        shadowOffset: { width: 0, height: 1 },
-        shadowOpacity: 0.3,
-        shadowRadius: 3,
-    },
-    placeholder: {
-        width: 50, // Match the width of the button
-    },
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+  header: {
+    ...card,
+    alignItems: "center",
+    justifyContent: "space-between",
+    marginBottom: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: Colors.orange_fonce,
+  },
+  iconButton: {
+    padding: 8,
+    borderRadius: 4,
+    backgroundColor: "#f0f0f0",
+    marginRight: 12,
+  },
+  row: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  actionButton: {
+    flex: 1,
+    padding: 16,
+    borderRadius: 4,
+    backgroundColor: Colors.orange_fonce,
+    alignItems: "center",
+    marginHorizontal: 4,
+  },
+  actionButtonText: {
+    fontSize: 16,
+    color: "#fff",
+  },
+  outlineButton: {
+    backgroundColor: "#f0f0f0",
+  },
+  outlineButtonText: {
+    fontSize: 16,
+    color: Colors.orange_fonce,
+  },
+  button: {
+    borderRadius: 10,
+    backgroundColor: "#fff",
+    padding: 10,
+    shadowColor: Colors.gris_fonce,
+    shadowOffset: { width: 0, height: 1 },
+    shadowOpacity: 0.3,
+    shadowRadius: 3,
+  },
+  placeholder: {
+    width: 50, // Match the width of the button
+  },
 });
-
